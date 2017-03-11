0c899c06845884ed8b6957e6eea814569dedbd79
Anat Abdullagasim <anat.pantera@gmail.com>
2017-03-11 15:52:44 +0700

    format code

--- src/test/java/com/anat/selenium/OpenBrowserTest.java
+++ src/test/java/com/anat/selenium/OpenBrowserTest.java
@@ -14,33 +14,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenBrowserTest {
-	
-	private static WebDriver driver;
-	
-	@BeforeClass
-	public static void setupBrowser() throws Exception {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver"); //path to chrome or internet Explorer driver
-		driver = new ChromeDriver(); // or new InternetExplorerDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // set timeout for load page
-	}
-	
-	@Test
-	public void testLoadPage() throws Exception {
-		driver.navigate().to("http://www.google.com"); // url to web page
 
-		//run your javascript and alert code for handle window
-		((JavascriptExecutor)driver).executeScript("alert('Test')"); 
-		driver.switchTo().alert().accept();
-		
-		WebElement element = driver.findElement(By.name("q"));
-		
-		assertNotNull(element);
-	}
-	
-	@AfterClass
-	public static void closeBrowser() throws Exception {
-		Thread.sleep(3000);
-		driver.quit();
-	}
-	
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setupBrowser() throws Exception {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver"); //path to chrome or internet Explorer driver
+        driver = new ChromeDriver(); // or new InternetExplorerDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // set timeout for load page
+    }
+
+    @Test
+    public void testLoadPage() throws Exception {
+        driver.navigate().to("http://www.google.com"); // url to web page
+
+        //run your javascript and alert code for handle window
+        ((JavascriptExecutor) driver).executeScript("alert('Test')");
+        driver.switchTo().alert().accept();
+
+        WebElement element = driver.findElement(By.name("q"));
+
+        assertNotNull(element);
+    }
+
+    @AfterClass
+    public static void closeBrowser() throws Exception {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
 }
